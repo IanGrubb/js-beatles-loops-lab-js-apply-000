7ea6f8c479ce3abbdd65ebd781543f48dc1002f5
Ian Grubb <yangrubb@gmail.com>
2019-05-29 18:12:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (musicians, instruments) {
   var thing = [];
   for (var i = 0; i< musicians.length; i++) {
-    thing.push()
+    thing.push(musicians[i] + " plays " + instruments[i])
   }
 }
